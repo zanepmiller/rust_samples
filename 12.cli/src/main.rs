de3d4ff65af51d6970059ef5fbb8f3ff1ecd7d94
zanepmiller <102548769+zanepmiller@users.noreply.github.com>
2024-10-02 14:47:57 -0600

    Chapt.12 Command Line Interface (#2)
    * Collected CLI arguments.
    
    * Added file reading.
    
    * Set up Config struct to handle command line arguments.
    
    * Changed Config new -> build, added error handling.
    
    * Extracted functionality to library crate.
    
    * Added tests and search function. Un-stubbed run.
    
    * Added case-insensitive search.
    
    * Added CLI case insensitive option.
    
    * Changed errors to output to stderr.

--- 12.cli/src/main.rs
+++ 12.cli/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+
+use cli::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = cli::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
