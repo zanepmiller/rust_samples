4a96bd70f28c6913d80a67bf6c05d1a1104d2390
zmiller <zane@zanepmiller.com>
2024-10-29 15:23:36 -0600

    Introduction to channels.

--- 16.concurrency/src/main.rs
+++ 16.concurrency/src/main.rs
@@ -1,26 +1,14 @@
+use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
-        }
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
     });
 
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
-    }
-
-    handle.join().unwrap();
-
-    let v = vec![1, 2, 3];
-
-    let handle = thread::spawn(move || {
-        println!("Here's a vector: {v:?}");
-    });
-
-    handle.join().unwrap();
+    let received = rx.recv().unwrap();
+    println!("Got: {received}");
 }
