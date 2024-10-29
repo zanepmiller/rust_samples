b88e3efb06c4286c247bf30a045e2d96a0a53a25
zmiller <zane@zanepmiller.com>
2024-10-29 15:16:18 -0600

    Introduction to move with threads.

--- 16.concurrency/src/main.rs
+++ 16.concurrency/src/main.rs
@@ -15,4 +15,12 @@ fn main() {
     }
 
     handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle.join().unwrap();
 }
