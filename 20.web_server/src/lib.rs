a8340ead970c498f7d51da19ef2e985a3f99cc0e
zmiller <zane@zanepmiller.com>
2024-12-09 12:20:31 -0700

    Added channels to Worker thread.

--- 20.web_server/src/lib.rs
+++ 20.web_server/src/lib.rs
@@ -1,12 +1,16 @@
 use std::{error::Error,
     fmt,
-    thread};
+    thread,
+    sync::{mpsc,
+        Arc,
+        Mutex},};
 
 //  TODO: Implement a Worker builder that gracefully handles a thread creation
 //  failure. 
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 struct Worker {
@@ -14,9 +18,13 @@ struct Worker {
     thread : thread::JoinHandle<()>,
 }
 
+struct Job;
+
 impl Worker {
-    fn new(id : usize) -> Worker {
-        let thread = thread::spawn(|| ());
+    fn new(id : usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         Worker {id, thread}
     }
@@ -39,11 +47,15 @@ impl ThreadPool {
     /// The size is the number of threads in the pool.
     pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
         if size < 1 {return Err(PoolCreationError)}
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        Ok(ThreadPool {workers})
+        Ok(ThreadPool {workers, sender})
     }
 
     pub fn execute<F>(&self, f: F)
